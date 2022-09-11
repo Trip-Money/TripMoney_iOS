b37539e94303b481d7c3fa3c15a1f530c4dd27a2
JI SOO <dlwltn815@gmail.com>
2022-09-12 02:45:57 +0900

    [Feat✨] UIStackView 서브뷰 추가하는 extension 추가 (#8)

--- YeoJeong/Global/Extension/UIStackView+.swift
+++ YeoJeong/Global/Extension/UIStackView+.swift
@@ -5,4 +5,13 @@
 //  Created by LeeJiSoo on 2022/09/11.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+
+    func addArrangedSubviews(_ views: UIView...) {
+        for view in views {
+            addArrangedSubview(view)
+        }
+    }
+}

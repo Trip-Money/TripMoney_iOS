507864518eee576314e48c4c44ad26cd0af0e3dc
JI SOO <dlwltn815@gmail.com>
2022-09-12 03:52:18 +0900

    [Design🎨] expandable view 프로퍼티 생성 (#8)

---
+++ YeoJeong/Screens/CreateYJ/SupportingView/ExpandableView.swift
@@ -0,0 +1,39 @@
+//
+//  ExpandableView.swift
+//  YeoJeong
+//
+//  Created by LeeJiSoo on 2022/09/12.
+//
+
+import UIKit
+
+import SnapKit
+
+class ExpandableView: UIView {
+
+    // MARK: - view
+
+    private let contentView = UIView()
+
+    private let titleView: UIView = {
+        $0.backgroundColor = .green
+        return $0
+    }(UIView())
+
+    private let cellExpandButton: UIButton = {
+        $0.addTarget(nil, action: #selector(didButtonTap), for: .touchUpInside)
+        return $0
+    }(UIButton())
+
+    private let cellNameLabel: UILabel = {
+        $0.text = "여행지 설정"
+        $0.font = .systemFont(ofSize: 22.0, weight: .bold)
+        return $0
+    }(UILabel())
+
+    private let dropdownView: UIImageView = {
+        $0.contentMode = .scaleToFill
+        $0.backgroundColor = .red
+        return $0
+    }(UIImageView())
+}

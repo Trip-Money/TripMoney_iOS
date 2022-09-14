d8469cee83cdd7f89a96ea17fcfd1bf85b0a8a9b
JI SOO <dlwltn815@gmail.com>
2022-09-14 22:36:11 +0900

    [Rename🚚] collectionviewcell로 변경
    - UIView로 개발하던 사항을 UICollectionView로 변경

---
+++ YeoJeong/Screens/CreateYJ/SupportingView/ExpandableCell.swift
@@ -0,0 +1,16 @@
+//
+//  ExpandableCell.swift
+//  YeoJeong
+//
+//  Created by LeeJiSoo on 2022/09/12.
+//
+
+import UIKit
+
+import SnapKit
+
+class ExpandableCell: UICollectionViewCell {
+
+    // MARK: - view
+
+}

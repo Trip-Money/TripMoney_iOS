9707f7d339e355b954986470bf308b1ab47ef44e
JI SOO <dlwltn815@gmail.com>
2022-09-20 01:23:05 +0900

    [Remove🔥] UIView로 방식 변경을 위해 코드 삭제 (#8)

--- YeoJeong/Screens/CreateYJ/CreateTravelView.swift
+++ YeoJeong/Screens/CreateYJ/CreateTravelView.swift
@@ -11,34 +11,9 @@ import SnapKit
 
 class CreateTravelView: BaseViewController {
 
-    // MARK: - View
-
-    private lazy var collectionView: UICollectionView = {
-        $0.delegate = self
-        $0.dataSource = self
-        $0.register(ExpandableCell.self, forCellWithReuseIdentifier: "expandableCell")
-        return $0
-    }(UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()))
-
-    private let expandableCell = ExpandableCell()
-    
-    // MARK: - Life Cycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: - base에 올려놓을 것임 노엘이 - 삭제하고 올리셈
         view.backgroundColor = UIColor.systemBackground
     }
-}
-
-// MARK: - UICollectionView Delegate
-
-extension CreateTravelView: UICollectionViewDelegateFlowLayout {
-
-}
-
-// MARK: - UICollectionView DataSource
-
-extension CreateTravelView: UICollectionViewDataSource {
-
 }

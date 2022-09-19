00e6b837449eed25559250ab8a70ad4ddc75798c
JI SOO <dlwltn815@gmail.com>
2022-09-20 00:46:26 +0900

    [Rename🚚] CreateTravelView로 이름 변경 (#8)

---
+++ YeoJeong/Screens/CreateYJ/CreateTravelView.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTravelView.swift
+//  YeoJeong
+//
+//  Created by LeeJiSoo on 2022/09/11.
+//
+
+import UIKit
+
+import SnapKit
+
+class CreateTravelView: BaseViewController {
+
+    // MARK: - View
+
+}
+

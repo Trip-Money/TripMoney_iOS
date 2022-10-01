91ef8d1c8553f8ee3f891a836803f91c2b5cb084
GODNOEL <ah2650@naver.com>
2022-10-01 19:25:56 +0900

    [Design🎨] 네비게이션 바 UI 수정 (#16)
    - 네비게이션 아이콘 변경 및 아이콘 색상 수정

--- YeoJeong/Global/Supports/AppDelegate.swift
+++ YeoJeong/Global/Supports/AppDelegate.swift
@@ -13,10 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        let navigationBarAppearnce = UINavigationBar.appearance()
+        navigationBarAppearnce.tintColor = UIColor.black
+        
         return true
     }
 
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
